1b79b106b4cc6bb94f9db518397a96ededb0fd14
MelvinEliu <melvingarciahe@gmail.com>
2026-04-16 12:02:02 -0600

    Segundo Commit

---
+++ Prueba.py
@@ -0,0 +1,16 @@
+from ClaseBasePokemon import Pokemon    
+
+pokemon1 = Pokemon("Pikachu", 100, 50)
+
+pokemon1.hp_actual = -2000
+
+pokemon1.mostrar_hp()
+
+pokemon1.hp_actual = 200
+
+pokemon1.mostrar_hp()
+
+
+pokemon1.hp_actual = 50
+
+pokemon1.mostrar_hp()
