8c20ccd3cc762feb5cc179bf4d73fe5c84fb8c28
Melvin Eliu <melvingarciahe@gmail.com>
2026-04-22 04:47:54 -0600

    Resultado Final

--- main.py
+++ main.py
@@ -98,4 +98,130 @@ def turno(pokemon, oponente):
         except ValueError:
             print("Debes ingresar un número válido.")
 
+#Aca agregamos los datos del turno automatico.
+def turno_computadora(pokemon, oponente):
+
+    print(f"\n--- TURNO DE {pokemon.nombre} (Computadora) ---")
+
+    # Iniciamos con un condicional para verificar si el pokemon esta paralizado
+    if pokemon.paralizado:
+        pokemon.mostrar_estado()
+        print(f"{pokemon.nombre} está paralizado y pierde su turno.")
+        #Si el pokemon esta paralizado lo cambiamos a false para cambiar su estado.
+        pokemon.paralizado = False
+        return
+    
+    pokemon.mostrar_estado()
+
+    #IMPORTANTE, uso de la IA Para conocimiento de la libreria random,
+    #random.randint me genera un numero aleatorio entre los valores indicados en parentesis
+    opcion = random.randint(1, 3)
+
+    print(f"La computadora elige: {opcion}")
+
+    #Usamos if para verificar el dato elegido aleatoriamente e inicializar los metodos para el valor elegido
+
+    if opcion == 1:
+        pokemon.atacar(oponente)
+
+    elif opcion == 2:
+        pokemon.defender()
+
+    else:
+        pokemon.descansar()
+
+
+#Creamos el metodo de combate, vamos a indicarle que necesitamos dos parametros jugador 1
+#Agregamos un tercer parametro contra_compu, parametro por defecto 
+def combate(pokemon_jugador1, pokemon_jugador2, contra_computadora=False):
+
+    print("\n¡COMIENZA LA BATALLA!")
+    print(f"{pokemon_jugador1.nombre} vs {pokemon_jugador2.nombre}")
+
+    #Declaramos una variable para identificar a quien le toca jugar
+    turno_actual = "jugador1"
+
+    #Creamos un ciclo while para que los jugadores puedan jugar mientras se cumpla la condicion
+    while pokemon_jugador1.hp_actual > 0 and pokemon_jugador2.hp_actual > 0:
+
+        print("\n----------------------------------------")
+
+        #Creamos el if, que nos indique si esta el jugador 1, que use el metodo turno, que contiene pokemon y oponente
+
+        if turno_actual == "jugador1":
+            #Usamos el metodo turno y le damos los parametros, pokemon = pokemon_jugador1
+            turno(pokemon_jugador1, pokemon_jugador2)
+            #Cambiamos el valor de turno para que le toque el turno al siguiente jugador
+            turno_actual = "jugador2"
+
+        else:
+            #Usamos nuestro tercer parametro el cual por ser booleano indicamos contracompu:
+            if contra_computadora:
+                #Corremos el metodo turno computadora
+                turno_computadora(pokemon_jugador2, pokemon_jugador1)
+            else:
+                #jugagor2
+                turno(pokemon_jugador2, pokemon_jugador1)
+
+            #Le devolvemos el valor al siguiente jugador
+            turno_actual = "jugador1"
+
+    #Se cierra el ciclo while e indicamos al ganador
+        # RESULTADO FINAL
+    print("\n========== RESULTADO ==========")
+
+    #Con un ciclo if identificamos si el jugador1 tiene puntos de vida lo cual siginfica es ganador.
+    #Sino imprimos jugador2
+    if pokemon_jugador1.hp_actual > 0:
+        print(f"{pokemon_jugador1.nombre} gana la batalla!")
+    else:
+        print(f"{pokemon_jugador2.nombre} gana la batalla!")
+
+#Definimos nuestra funcion para mostrar el menu
+def main():
+
+    print("======================================")
+    print("   SIMULADOR DE BATALLAS POKÉMON")
+    print("======================================")
+
+    #Modos de juego
+    print("1. Jugador vs Jugador")
+    print("2. Jugador vs Computadora")
+
+    #Agregamos la variable que guardara el dato ingresado por el jugador
+    modo = input("Selecciona modo: ")
+
+    # llamamos a la funcion importada de pokedex, para mostrar el catalogo
+    mostrar_catalogo_disponible()
+
+    # Creamos la variable donde se guardara el pokemon, por lo cual llamamos al metodo elegir pokemon.
+    #Le dejamos el nombre pokemon_jugador1 ya que es una variable y no afecta que sea repetido con el nombre de un parametro
+    pokemon_jugador1 = elegir_pokemon(1)
+
+    #Condicional si el jugador elige 2, el cpu elige aleatoriamente
+
+    if modo == "2":
+        #Uso de IA para corregir el uso de random.choice - uso de la libreria random
+        #al utilizar el diccinario.values, nos devuelve los valores, sin la clave.
+        #Usamos list, para crear esos valores en una lista
+        #Con la lista creada podemos proceder a utilizar random.choice y que nos brinde un valor de las listas
+        datos_computadora = random.choice(list(CATALOGO_POKEMON.values()))
+
+        #Creamos la variable donde se guardara el pokemon y llamamos al metodo de crear_pokemon
+        #nos brindara datos de diccionario los cuales convertiremos conforme su clase
+        pokemon_jugador2 = crear_pokemon(datos_computadora)
+        print(f"La computadora eligió a {pokemon_jugador2.nombre}")
+
+
+        #llamamos al metodo de combate y corremos TRUE contracomputadora
+        combate(pokemon_jugador1, pokemon_jugador2, True)
+
+    else:
+        # Jugador 2
+        pokemon_jugador2 = elegir_pokemon(2)
+        combate(pokemon_jugador1, pokemon_jugador2)
+
+#Si este archivo es el principal → ejecuta main()
+if __name__ == "__main__":
+    main()
 
